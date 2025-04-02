4f8e338634b35d55be8af5be0130680f5ab9080f
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-04-02 13:38:50 -0500

    fix(deps): hmtl forms module

--- packages/package-template/tsup.config.ts
+++ packages/package-template/tsup.config.ts
@@ -3,19 +3,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
   entry: ['./src/index.ts'],
-  target: 'es5',
+  target: 'esnext',
   format: ['esm'],
   dts: true,
   clean: true,
   splitting: true,
   treeshake: true,
-  external: [
-    'framer-motion',
-    'react',
-    'react-dom',
-    'tailwind',
-    'tailwind-variants',
-    'tailwindcss-animate',
-  ],
+  external: ['react', 'react-dom'],
   ...options,
 }));
